6ec38d61f395ae04bb8757bff3006152ec0fbe5e
Jhan24b <jhan24b@gmail.com>
2024-01-24 10:49:12 -0500

    logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,9 @@ const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 export const metadata = {
   title: "Pyye",
   description: "Enjoy a tasty pye",
+  icons: {
+    icon: "favicon.ico", 
+  },
 };
 
 export default function RootLayout({ children }) {
